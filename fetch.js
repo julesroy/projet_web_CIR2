3a3bdcb9c780ac69a6fe8b04de15077e2efccf8a
Jules ROY <jules.roy@student.junia.com>
2024-04-11 09:42:47 +0200

    améliorations graphiques des pages d'authentification, du header, et remise des images pour le classement

--- fetch.js
+++
@@ -1,5 +0,0 @@
-fetch(
-    "https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed>=315"
-)
-    .then((response) => response.json())
-    .then((jsonContent) => console.log(jsonContent));
